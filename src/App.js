eb09ffd571cad85f25849a836c5d594013bb4d3a
Marwa Refaieh <marwarefaieh@gmail.com>
2024-10-09 16:17:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const initialState = 0;
+
+  const reducer = (state, action) => {
+    switch (action) {
+      case "inc":
+        return state + 1;
+      case "dec":
+        return state - 1;
+      case "reset":
+        return initialState ;
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return <div className="App">
+    <h2>{state}</h2>
+    <button onClick={()=>{dispatch('inc')}}>+</button>
+    <button onClick={()=>{dispatch('dec')}}>-</button>
+    <button onClick={()=>{dispatch('reset')}}>Reset</button>
+  </div>;
 }
 
 export default App;
